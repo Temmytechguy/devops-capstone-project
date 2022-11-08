e465e2ac0d913673843c0b1399e87e2564b8488e
Temmytechguy <temidayofolorunsho9@gmail.com>
2022-11-08 17:21:06 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -23,6 +23,7 @@ def health():
 ######################################################################
 # GET INDEX
 ######################################################################
+
 @app.route("/")
 def index():
     """Root URL response"""
@@ -35,9 +36,12 @@ def index():
         status.HTTP_200_OK,
     )
 
+
+
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -55,17 +59,18 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 
 #place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
-    List all Accounts
-    This endpoint will list all Accounts
+        List all Accounts
+        This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
@@ -73,11 +78,11 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-#place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -91,11 +96,10 @@ def read_account(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
-
 #place you code here to UPDATE an account...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -112,11 +116,10 @@ def update_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-# ... place you code here to DELETE an account ...
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
@@ -129,6 +132,8 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return account.serialize(), status.HTTP_204_NO_CONTENT
+
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
