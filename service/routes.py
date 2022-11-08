b0621540ec4889a68df6df055d63512424aab8e7
Temmytechguy <temidayofolorunsho9@gmail.com>
2022-11-08 17:51:34 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -13,6 +13,7 @@ from . import app  # Import Flask application
 # Health Endpoint
 ############################################################
 
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -59,7 +60,7 @@ def create_accounts():
 
 
 ######################################################################
- # LIST ALL ACCOUNTS
+#  LIST ALL ACCOUNTS
 ######################################################################
 
 @app.route("/accounts", methods=["GET"])
@@ -75,10 +76,10 @@ def list_accounts():
     return jsonify((account_list), status.HTTP_200_OK)
 
 
+######################################################################
+#  READ AN ACCOUNT
+######################################################################
 
-######################################################################
-# READ AN ACCOUNT
-######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
@@ -93,7 +94,7 @@ def read_account(account_id):
 
 
 ######################################################################
-# UPDATE AN EXISTING ACCOUNT
+#  UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -111,7 +112,7 @@ def update_accounts(account_id):
 
 
 ######################################################################
-# DELETE AN ACCOUNT
+#  DELETE AN ACCOUNT
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
