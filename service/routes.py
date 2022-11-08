dd96687f548564ade75e9899a49fc574ada64b2d
Temmytechguy <temidayofolorunsho9@gmail.com>
2022-11-08 16:39:57 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -1,7 +1,8 @@
-"""
-Account Service
 
-This microservice handles the lifecycle of Accounts
+
+"""
+    Account Service
+    This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
 from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
@@ -78,13 +79,14 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_account(account_id):
     """
         Reads an Account
         This endpoint will read an Account based the account_id that is requested
-        """
+    """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -98,22 +100,19 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
-
+#... place you code here to UPDATE an account...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
-    Update an account
-    This endpoint will update an Account based on the posted data
+        Update an account
+        This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account")
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
     account.deserialize(request.get_json())
     account.update()
-
     return account.serialize(), status.HTTP_200_OK
 ######################################################################
 # DELETE AN ACCOUNT
@@ -129,13 +128,12 @@ def delete_accounts(account_id):
         Delete an Account
         This endpoint will delete an Account based on the account_id that is requested
         """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
 
-        account = Account.find(account_id)
-        if account:
-            account.delete()
-
-        return "", status.HTTP_204_NO_CONTENT
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
