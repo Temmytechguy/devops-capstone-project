4427a38c30a00a673b9e48731bb9db68d8210aad
Temmytechguy <temidayofolorunsho9@gmail.com>
2022-11-08 17:07:54 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -1,5 +1,4 @@
 
-
 """
     Account Service
     This microservice handles the lifecycle of Accounts
@@ -14,6 +13,7 @@ from . import app  # Import Flask application
 ############################################################
 # Health Endpoint
 ############################################################
+
 @app.route("/health")
 def health():
     """Health Status"""
@@ -34,7 +34,6 @@ def index():
         ),
         status.HTTP_200_OK,
     )
-
 
 ######################################################################
 # CREATE A NEW ACCOUNT
@@ -61,6 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
@@ -91,14 +91,13 @@ def read_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     return account.serialize(), status.HTTP_200_OK
-
 
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 #... place you code here to UPDATE an account...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
@@ -114,26 +113,25 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
+
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-    ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-        """
+    """
         Delete an Account
         This endpoint will delete an Account based on the account_id that is requested
-        """
+    """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
         account.delete()
-
-    return "", status.HTTP_204_NO_CONTENT
+    return account.serialize(), status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
