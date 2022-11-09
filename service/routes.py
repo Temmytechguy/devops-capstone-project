aee3473c245705e2b68a888dbf28b6a67d2c711e
Temmytechguy <temidayofolorunsho9@gmail.com>
2022-11-09 13:09:56 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -64,8 +64,8 @@ def create_accounts():
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
-    List all Accounts
-    This endpoint will list all Accounts
+        List all Accounts
+        This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
@@ -88,7 +88,6 @@ def read_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -119,17 +118,16 @@ def update_accounts(account_id):
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-        """
+    """
         Delete an Account
         This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
 
-        account = Account.find(account_id)
-        if account:
-            account.delete()
-
-        return "", status.HTTP_204_NO_CONTENT
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
